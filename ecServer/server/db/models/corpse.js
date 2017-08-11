be8a259ff640a481b47235e5de1d78f7246bea78
Kevin Genus <kevin.genus@gmail.com>
2017-08-11 16:29:44 -0400

    feature: adds Corpse model

---
+++ ecServer/server/db/models/corpse.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Corpse = db.define('corpse', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    totalCells: {
+        type: Sequelize.INTEGER,
+        defaultValue: 3
+    },
+    complete: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+    },
+})
+
+module.exports = Corpse
