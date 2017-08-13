90397f8f386499a6da02e8941bb507e9fb956428
Shayne Mihalka <shayne@mihalka.com>
2017-08-12 19:28:12 -0700

    feature: Home Screen Starter

--- ecMobileApp/components/Home.js
+++ ecMobileApp/components/Home.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, ScrollView, View, Image } from 'react-native'
 
 export default class Home extends Component {
   render () {
     return (
-      <View>
-        <Text>HomeView</Text>
+      <View style={styles.container}>
+        <ScrollView>
+          {[0, 1, 2].map((index) => {
+            return (<View key={index} style={styles.corpse}>
+              <View style={styles.imageCorpseTop}/>
+              <Image
+                style={styles.imageCorpse}
+                source={require('../public/images/corpse.jpg')}
+              />
+              <View style={styles.imageCorpseBottom}/>
+            </View>)
+          })}
+        </ScrollView>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  corpse: {
+    width: 340,
+    height: 400,
+    backgroundColor: 'powderblue',
+    marginTop: 15,
+    marginBottom: 15,
+    alignItems: 'center'
+  },
+  imageCorpseTop: {
+    backgroundColor: 'steelblue',
+    width: 340,
+    height: 30
+  },
+  imageCorpseBottom: {
+    backgroundColor: 'steelblue',
+    width: 340,
+    height: 30
+  },
+  imageCorpse: {
+    width: 340,
+    height: 340,
+    alignItems: 'center'
+  }
+})
