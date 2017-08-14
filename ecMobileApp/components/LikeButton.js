0b80ba463065fb870a772ccf9938dff93c5c467b
Shayne Mihalka <shayne@mihalka.com>
2017-08-13 18:11:49 -0700

    lots of test features to many to not in this line

---
+++ ecMobileApp/components/LikeButton.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { View, TouchableOpacity, Text } from 'react-native'
+
+export default class LikesButton extends Component {
+  render () {
+    let like = <Text>{this.props.likes} Likes</Text>
+    if (this.props.userLike) {
+      like = <Text style={this.props.style.textLikedCorpse}>{this.props.likes} Likes</Text>
+    }
+    return (
+      <View>
+        <TouchableOpacity
+          onPress={() => this.props.handleLike(this.props.corpseId)}
+        >
+          <View>
+            {like}
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
