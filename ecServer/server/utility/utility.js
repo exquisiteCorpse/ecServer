1b4a04d0fd1a8a7860d011e8bea22916282f310c
Kevin Genus <kevin.genus@gmail.com>
2017-08-16 04:21:18 -0400

    adds utility functions

---
+++ ecServer/server/utility/utility.js
@@ -0,0 +1,19 @@
+/***
+ * mergePhotos - creates the Corpse static image file
+ * @param corpseId      corpseId
+ * @param corpsePath    path to corpseId's directory
+ * @param appendValue   -append for vertical, +append for horizontal
+ */
+const im = require('imagemagick')
+const mergePhotos = (corpseId, corpsePath, appendValue = '-append') => {
+  const imagesToConvert = [appendValue,
+    `${corpsePath}/${corpseId}-top.jpg`,
+    `${corpsePath}/${corpseId}-middle.jpg`,
+    `${corpsePath}/${corpseId}-bottom.jpg`,
+    `${corpsePath}/ExquisiteCorpse${corpseId}.jpg`]
+  im.convert(imagesToConvert, (err) => {
+    if (err) throw console.error('generateCorpsImage', err)
+  })
+}
+
+export {mergePhotos}
