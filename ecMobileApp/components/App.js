2b3c0eb427ab18edba74b7e957df91fd79dfbe1f
Lina Jones <lina.jones630@gmail.com>
2017-08-15 22:51:48 -0600

    (feat): facebook login in progress-just need login button

--- ecMobileApp/components/App.js
+++ ecMobileApp/components/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
-import { AppRegistry, StyleSheet, View, Text } from 'react-native'
-import Camera from './Camera/AppCamera'
+import { AppRegistry, StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import FBSDK, { LoginManager } from 'react-native-fbsdk'
 
 export default class App extends Component {
+
+  // _fbAuth() {
+  //   LoginManager.logInWithPublishPermissions(['public_profile']).then((result) => {
+  //     if (result.isCancelled) console.log('Login was cancelled')
+  //     else console.log('Login was a success ' + result.grantedPermissions.toString())
+  //   }, (error) => {
+  //     console.log('An error occured: ' + error)
+  //   })
+  // }
+// <TouchableOpacity onPress={this._fbAuth()}>
+//   <Text>Facebook</Text>
+// </TouchableOpacity>
+
   render () {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to Exquisite Corpse!
+
         </Text>
       </View>
     )
