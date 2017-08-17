98b49cd5ece3ee137e742a454b053218e22fa1f0
Lina Jones <lina.jones630@gmail.com>
2017-08-17 16:36:58 -0600

    (feat) facebook login button

--- client/components/main.js
+++ client/components/main.js
@@ -1,42 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {withRouter, Link} from 'react-router-dom'
-import {logout} from '../store'
-
+import { connect } from 'react-redux'
+import { withRouter, Link } from 'react-router-dom'
+import { logout } from '../store'
+import FacebookLogin from 'react-facebook-login'
+import { facebook } from '../../secrets'
 /**
  * COMPONENT
  *  The Main component is our 'picture frame' - it displays the navbar and anything
  *  else common to our entire app. The 'picture' inside the frame is the space
  *  rendered out by the component's `children`.
  */
-const Main = (props) => {
-  const {children, handleClick, isLoggedIn} = props
+export class Main extends Component {
 
-  return (
-    <div>
-      <h1>Exquisite Corpse</h1>
-      <nav>
-        {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-            </div>
-        }
-      </nav>
-      <hr />
-      {children}
-    </div>
-  )
+  responseFacebook (response) {
+    console.log(response)
+  }
+  render () {
+
+    const { children, handleClick, isLoggedIn } = this.props
+    const facebookcli = facebook.clientID
+    return (
+      <div>
+        <h1>Exquisite Corpse</h1>
+        <nav>
+          {
+            isLoggedIn
+              ? <div>
+                {/* The navbar will show these links after you log in */}
+                <Link to="/home">Home</Link>
+                <a href="#" onClick={handleClick}>Logout</a>
+              </div>
+              : <div>
+                {/* The navbar will show these links before you log in */}
+                <Link to="/login">Login</Link>
+                <Link to="/signup">Sign Up</Link>
+                <FacebookLogin
+                  appId={facebookcli}
+                  autoLoad={true}
+                  fields="name,email,picture"
+                  scope="public_profile,user_friends,user_actions.books"
+                  callback={this.responseFacebook}
+                />
+              </div>
+          }
+        </nav>
+        <hr />
+        {children}
+      </div>
+    )
+  }
 }
-
 /**
  * CONTAINER
  */
@@ -48,7 +62,7 @@ const mapState = (state) => {
 
 const mapDispatch = (dispatch) => {
   return {
-    handleClick () {
+    handleClick() {
       dispatch(logout())
     }
   }
