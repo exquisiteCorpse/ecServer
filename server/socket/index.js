9607e493283cd2f3f936a243be78f6c1696cf4a0
Shayne Mihalka <shayne@mihalka.com>
2017-08-22 18:09:45 -0700

    sockets: socket test -> should brodcast a message that tells other connected devices to update their likes

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,6 +2,11 @@ module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
+    socket.on('message', (message)=> {
+      console.log(message)
+      socket.broadcast.emit('message', message)
+    })
+
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
