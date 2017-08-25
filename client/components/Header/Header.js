daf44be7e9663cdc656afd11d198ab07903a682f
Fara Woolf <fbwoolf@gmail.com>
2017-08-25 16:03:53 -0500

    style: fix

--- client/components/Header/Header.js
+++ client/components/Header/Header.js
@@ -2,27 +2,29 @@ import React from 'react'
 import {connect} from 'react-redux'
 import styled from 'styled-components'
 
+// background-image: url('images/guy.jpg');
+
 const Header = styled.section`
   {
     position: fixed;
-    height: 200px;
+    height: 100px;
     width: 100%;
-    top: 50px;
+    top: 80px;
     background-color: ${props => props.theme.white};
     border-bottom: 1px solid ${props => props.theme.darkgrey};
     display: flex;
     align-items: center;
 
     .header-message {
-      font-size: 2rem;
       color: ${props => props.theme.darkgrey};
       margin: 0 auto;
       display: flex;
       align-items: center;
 
-      h1 {
+      h3 {
+        font-size: 2.5rem;
+        font-weight: bold;
         margin: 0;
-        float: left;
       }
 
       img {
