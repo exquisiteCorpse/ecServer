ad8cb5b510f685a038a23ee790530caa3aaba62d
Shayne Mihalka <shayne@mihalka.com>
2017-09-10 09:18:31 -0700

    feature: Add friends in server api

--- server/api/friends.js
+++ server/api/friends.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { User } = require('../db/models')
+const db = require('../db')
 module.exports = router
 
 router.get('/:userId', (req, res, next) => {
@@ -8,5 +9,17 @@ router.get('/:userId', (req, res, next) => {
     include: [{ model: User, as: 'friend' }]
   })
     .then(user => res.json(user.friend))
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+  db.model('Friend').create(req.body)
+    .then(friend => res.json(friend))
+    .catch(next)
+})
+
+router.delete('/', (req, res, next) => {
+  db.model('Friend').destroy({where: req.body})
+    .then(friend => res.json(friend))
     .catch(next)
 })
